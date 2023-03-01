f393388d7dd2c762cb27bfc7430c7115b644107c
victoryuen <dhuce3@gmail.com>
2023-03-01 14:23:14 -0500

    Configure React app for deployment to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+function AboutPage() {
+  return (
+    
+    <div>
+      <img className="avatar" />
+      <h1>About Me</h1>
+      <h2>Victor Yuen</h2>
+      <p>HEYYYYYYY <br /> IT WORKS WOOOOO</p>
+
+      </div>
+  );
+}
+
+export default function MyApp() {
+  return (
+    
+    <div>
+      <AboutPage />
+      <h1>Welcome to my app</h1>
+      
+    </div>
+  );
+}
