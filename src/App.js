166d916ed337c2b956d393c755740d2ddd685ad3
victoryuen <dhuce3@gmail.com>
2023-03-01 20:38:16 -0500

    image adding

--- src/App.js
+++ src/App.js
@@ -22,24 +22,50 @@ function App() {
     </div>
   );
 }
+const user = {
+  name: 'Victor Yuen',
+  imageUrl: 'https://i.stack.imgur.com/l60Hf.png',
+  imageWidth: 100,
+  imageHeight: 100,
+};
+
+const products = [
+  { title: 'Strawberry', id: 1 },
+  { title: 'Tacos', id: 2 },
+  { title: 'Steak', id: 3 },
+];
+
 function AboutPage() {
   return (
     
-    <div>
-      <img className="avatar" />
-      <h1>About Me</h1>
-      <h2>VictorYuen</h2>
-      <p>HEYYYYYYY <br /> IT WORKS WOOOOO</p>
+    <div style={{ textAlign: 'center' }}>
+      <h1>{user.name}</h1>
+      <img
+       className = "avatar"
+      src={user.imageUrl}
+      style={{
+        width: user.imageWidth,
+        height: user.imageHeight
+      }}
+  ></img>
+      <h1>wdawd</h1>
+      <h2>About Me</h2>
+      <p>Hunter College <br /> Computer Science Major</p>
 
       </div>
   );
 }
-
+function TastyFoodList(){
+  const listItems = products.map(products =>
+    <li key={products.id}>{products.title}</li>
+    )
+}
 export default function MyApp() {
   return (
     
     <div>
       <AboutPage />
+      <TastyFoodList />
       <h1>Welcome to my app</h1>
       
     </div>
